a4b834ee5dd89f45007a686540c6ad3b36021b7e
Yang Bai <cedriccan48@gmail.com>
2018-05-17 11:01:07 +0800

    committed

--- scrape.js
+++ scrape.js
@@ -1,8 +1,13 @@
 const scrapeIt = require("scrape-it");
-
+const fs = require('fs');
 var all_info = [];
 
 var inorder = [];
+try{
+  fs.mkdirSync('data');
+} catch(err) {
+  console.log("The 'data' folder already exists");
+}
 var url = "http://shirts4mike.com/shirt.php";
 
 function formatDate() {
@@ -21,7 +26,7 @@ var the_date = formatDate();
 var the_file_name = "data/" + the_date + ".csv"; // the csv file path
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const csvWriter = createCsvWriter({
-	append: true,
+//	append: true,
     path: the_file_name,
     header: [
         {id: 'title', title: 'Title'},
@@ -31,55 +36,59 @@ const csvWriter = createCsvWriter({
         {id: 'time', title: 'Time'},
     ]
 });
+
 scrapeIt(url, {
+    title: "header title"
+}).then(({ data, response }) => {
+    scrapeIt(url, {
     // Fetch the shirts infomation
     shirts: {
         listItem: ".products li"
       , data: {
  
           title: {
-          	selector: "img",
-          	attr: "alt"
+            selector: "img",
+            attr: "alt"
           }
           , link: {
               selector: "a",
               attr: "href"
            }
           , image: {
-		        selector: "li img"
-		      , attr: "src"
+            selector: "li img"
+          , attr: "src"
            }
         }
     }
 }, (err, { data }) => {
-	data["shirts"].forEach(function(every){
-		every.link = "http://shirts4mike.com/" + every.link;
-		every.image = "http://shirts4mike.com/" + every.image;
-	})
-	all_info = data["shirts"].slice();
-	//get the price for each shirt
+  data["shirts"].forEach(function(every){
+    every.link = "http://shirts4mike.com/" + every.link;
+    every.image = "http://shirts4mike.com/" + every.image;
+  })
+  all_info = data["shirts"].slice();
+  //get the price for each shirt
     for(let i=0; i<8; i++)
     {
-	    scrapeIt(all_info[i].link, {
-	    title: "head title"
-	  , price: ".shirt-details h1 span"
-	  , image: {
-	        selector: ".shirt-picture span img"
-	      , attr: "src"
-	    }
-	}).then(({ data, response }) => {
-	    all_info[i]["price"] = data["price"];
-	    all_info[i]["time"] = new Date();
-	    var new_set = {};
-	    //keep the records in this order: title, price, imageURL, URL, time
-	    new_set["title"] = all_info[i]["title"];
-	    new_set["price"] = all_info[i]["price"];
-	    new_set["image"] = all_info[i]["image"];
-	    new_set["link"] = all_info[i]["link"];
-	    new_set["time"] = all_info[i]["time"];
-	    inorder.push(new_set)
-	    if(i === 7){
-	    csvWriter.writeRecords(inorder)       // returns a promise
+      scrapeIt(all_info[i].link, {
+      title: "head title"
+    , price: ".shirt-details h1 span"
+    , image: {
+          selector: ".shirt-picture span img"
+        , attr: "src"
+      }
+  }).then(({ data, response }) => {
+      all_info[i]["price"] = data["price"];
+      all_info[i]["time"] = new Date();
+      var new_set = {};
+      //keep the records in this order: title, price, imageURL, URL, time
+      new_set["title"] = all_info[i]["title"];
+      new_set["price"] = all_info[i]["price"];
+      new_set["image"] = all_info[i]["image"];
+      new_set["link"] = all_info[i]["link"];
+      new_set["time"] = all_info[i]["time"];
+      inorder.push(new_set)
+      if(inorder.length === 8){
+      csvWriter.writeRecords(inorder)       // returns a promise
         .then(() => {
         console.log(inorder);
         });
@@ -87,3 +96,9 @@ scrapeIt(url, {
     });
   }
   })
+}).catch(function(err) {
+    console.log("Error code:" + err.code);
+})
+
+
+
